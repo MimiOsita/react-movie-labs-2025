44968002fb6bb36a84606643b436e699b240da78
MimiOsita <20097663@mail.wit.ie>
2025-11-02 17:41:30 +0000

    Added Now Playing Movies Page

---
+++ movies/src/pages/nowPlayingMoviesPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getNowPlayingMovies } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
+import Spinner from "../compoents/spinner";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+
+
+const NowPlayingMoviesPage = (props) => {
+    const { data, error, isPending, isError } = useQuery({
+        queryKey: ["now_playing"],
+        queryFn: getNowPlayingMovies,
+    });
+
+    if (isPending)
+        return <Spinner/>;
+
+    if (isError)
+        return <h1>{error.message}</h1>
+
+    const movies = data.results;
+
+    return (
+        <PageTemplate
+        title="Now Playing"
+        movies={movies}
+        action={(movie) => <AddToFavoritesIcon movie={movie} /> }
+        />
+    );
+};
+
+export default NowPlayingMoviesPage;
